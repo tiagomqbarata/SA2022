60230a04f8023b8234bcdc30e4fc59c2eb4c2377
tiagomqbarata <tiagobarata98@gmail.com>
2022-05-03 10:33:12 +0100

    novos dados

--- json_cvs.py
+++ json_cvs.py
@@ -1,14 +1,31 @@
 import pandas as pd
+import datetime as dt
 
 df = pd.read_json (r'data.json')
 df_transposed = df.T
-df_transposed["index"] += 1 
+df_transposed["index"] = [a for a in range(df_transposed.confidence.count())]
 df_transposed = df_transposed.set_index(['index'])
 
 for elem in df_transposed.local.unique():
     print(elem + " - " + str(len(df_transposed[df_transposed.local == elem])))
 
 df = df_transposed
+
+def date_timeToRecord_date(value):
+    # 2019-02-13 23:00:00
+    return dt.datetime.strptime(value,'%m-%d-%Y_%H:%M:%S')
+     
+
+df["record_date"] = df["date_time"].map(lambda x: date_timeToRecord_date(x))
+df.drop("date_time", axis=1, inplace=True)
+
+
+df.confidence = df.confidence.map(lambda x: float(x))
+df.currentSpeed = df.currentSpeed.map(lambda x: int(x))
+df.freeFlowSpeed = df.freeFlowSpeed.map(lambda x: int(x))
+df.currentTravelTime = df.currentTravelTime.map(lambda x: int(x))
+df.freeFlowTravelTime = df.freeFlowTravelTime.map(lambda x: int(x))
+
 
 df.currentSpeed == df.freeFlowSpeed & df.currentTravelTime == df.freeFlowTravelTime
 
@@ -21,6 +38,7 @@ df.drop('flowTravelTime', inplace=True, axis=1)
 
 df.loc[df["transito"] == "Sem Transito", "transito"] = 0
 
-df["transito"] = 1- (df.currentSpeed / df.freeFlowSpeed + df.freeFlowTravelTime / df.currentTravelTime  )/2
+df["transito"] = (1 - (df.currentSpeed / df.freeFlowSpeed + df.freeFlowTravelTime / df.currentTravelTime)/2) * df.confidence
 
 df_transposed.to_csv (r'data.csv', index=None)
+
